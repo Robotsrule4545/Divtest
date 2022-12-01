7eab92eb3751afbf2d7196944013d8d3fe987927
Robotsrule4545 <caleb.mincraft18@gmail.com>
2022-12-01 20:48:13 +0800

    Added movement, not working.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { dismissOverlay } = require("@ionic/core/dist/types/utils/overlays");
+
 const box = document.getElementById('moving');
 
 // Confirm it's working.
@@ -12,3 +14,24 @@ box.style.left = '150px';
 
 // Moving logic:
 
+document.addEventListener('keydown', movement, false);
+
+function movement(e) {
+    var keyCode = e.keyCode;
+    if (keycode == 87); {
+        box.style.top = int(box.style.top) + 1;
+        console.log(box.style.top);
+    }
+    elif (keyCode == 65); {
+        box.style.left = int(box.style.left) + 1;
+        console.log(box.style.left);
+    }
+    elif (keyCode == 83); {
+        box.style.top = box.style.top - 1;
+        console.log(box.style.down);
+    }
+    elif (keyCode == 68); {
+        box.style.left = box.style.left - 1;
+        console.log(box.style.right);
+    }
+}
