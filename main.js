051511a7180ef556d1cc9ca7b89539a21b59d5d2
Robotsrule4545 <caleb.mincraft18@gmail.com>
2022-12-01 16:24:55 +0800

    Bala advices
    Fix bugs

--- main.js
+++ main.js
@@ -12,15 +12,3 @@ box.style.left = '150px';
 
 // Moving logic:
 
-document.addEventListener('w', function up(event) {
-    console.log('up!');
-});
-document.addEventListener('a', function left(event) {
-    console.log('left!');
-});
-document.addEventListener('s', function down(event){
-    console.log('down!');
-});
-document.addEventListener('d', function right(event){
-    console.log('right!');
-});
