f99fbd3d7767dcc2e0c48ba4f765342c6b0b03b6
Robotsrule4545 <caleb.mincraft18@gmail.com>
2022-12-01 16:02:11 +0800

    Added and tried fixing stuf

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-const box = document.getElementById('moving');
+Piconst box = document.getElementById('moving');
+
+// Confirm it's working.
 
 console.log('Working!');
 console.log('Made by robot0730.');
+
+// Setup the div
+
+box.style.top = '150px';
+box.style.left = '150px';
+
+// Moving logic:
+
+document.addEventListener('w', function up(event) {
+
+});
+document.addEventListener('a', function left(event) {
+
+});
+document.addEventListener('s', function down(event){
+
+});
+document.addEventListener('d', function right(event){
+
+});
